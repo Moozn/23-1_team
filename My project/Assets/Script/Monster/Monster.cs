73ef0291e0be2a1c8dce366fb39fafa591c60c34
Im Seungwan <rexio15846@gmail.com>
2023-06-15 01:29:22 +0900

    qwe

--- My project/Assets/Script/Monster/Monster.cs
+++ My project/Assets/Script/Monster/Monster.cs
@@ -21,13 +21,11 @@ public class Monster : MonoBehaviour
     private Animator anim;
     [SerializeField] private MonstrState state;
     [SerializeField] private Player player;
-    private Rigidbody rigid;
+    [SerializeField] private Rigidbody rigid;
     private NavMeshAgent agent;
     private float dis_Pattern1, dis_Pattern2, dis_Pattern3; //패턴거리
     private float player_dis;
     [SerializeField] private GameObject bullet;
-    [SerializeField] private GameObject bulletpos;
-    [SerializeField] private GameObject bulletpos2;
     private bool delay;
     private int min,max;
     private void Init()
@@ -37,7 +35,7 @@ public class Monster : MonoBehaviour
         Mob_Exp = 0;
         state = MonstrState.Idle;
         anim = GetComponent<Animator>();
-        rigid = GetComponent<Rigidbody>();
+     //  rigid = GetComponent<Rigidbody>();
         agent = GetComponent<NavMeshAgent>();
         delay = true;
         min = 0;
@@ -95,11 +93,14 @@ public class Monster : MonoBehaviour
         player.Add_Exp(Mob_Exp);
     }
     private void Lookat()
-    {
-        Vector3 rotate = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z) - (transform.position);
-        Quaternion newRotate = Quaternion.LookRotation(rotate);
-        rigid.rotation = Quaternion.Slerp(rigid.rotation, newRotate, 10 * Time.fixedDeltaTime);
-    }
+    {   
+        if(!state.Equals(MonstrState.Dash))
+        {
+            Vector3 rotate = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z) - transform.position;
+            Quaternion newRotate = Quaternion.LookRotation(rotate);
+            transform.rotation = Quaternion.Slerp (transform.rotation, newRotate, 2 * Time.deltaTime);
+        }
+    }  
     private void Exploration() //탐색
     {
         float dis = distance();
@@ -126,17 +127,18 @@ public class Monster : MonoBehaviour
     }
     IEnumerator Think()
     {
-            yield return new WaitForSeconds(0.4f);
+        yield return new WaitForSeconds(0.1f);
 
-            int ranpattern = Random.Range(min, max);
-            Debug.Log(ranpattern);
-            switch (ranpattern)
+        int ranpattern = Random.Range(min, max);
+        Debug.Log(ranpattern);
+        yield return new WaitForSeconds(0.5f);
+        switch (ranpattern)
             {
                 case 0:
                      StartCoroutine(Delay());
                     break;
                 case 1:
-                    StartCoroutine(Rush());
+                StartCoroutine(Rush());
                     break;
                 case 2:
                     StartCoroutine(Breth());
@@ -156,12 +158,14 @@ public class Monster : MonoBehaviour
     private void Update()
     {
         Exploration();
+        Lookat();
     }
     private void FixedUpdate()
     {
-        Lookat();
-      //  Fire();
-        if (state.Equals(MonstrState.Dash)) rigid.AddForce(transform.forward * 5, ForceMode.Impulse);
+        if (state.Equals(MonstrState.Dash))
+        {
+            rigid.AddForce(transform.forward * 500);
+        }
         if (state.Equals(MonstrState.Run)) Move();
         else StartCoroutine(MoveStop());
     }
@@ -178,11 +182,14 @@ public class Monster : MonoBehaviour
     }
     IEnumerator Rush()
     {
-       anim.SetBool("Dash", true);
+       
+        anim.SetBool("Dash", true);
+        agent.isStopped = false;
         state = MonstrState.Dash;
-        yield return new WaitForSeconds(0.5f);
-      //  state = MonstrState.Idle;
+        yield return new WaitForSeconds(1f);
+        state = MonstrState.Idle;
         anim.SetBool("Dash", false);
+        agent.isStopped = true;
         StartCoroutine(Think());
     }
     IEnumerator Delay()
