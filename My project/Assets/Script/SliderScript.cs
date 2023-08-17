440f6246f82eedf46ee50f0560d03af7d48b1761
Im Seungwan <rexio15846@gmail.com>
2023-08-17 20:11:03 +0900

    123

--- My project/Assets/Script/SliderScript.cs
+++ My project/Assets/Script/SliderScript.cs
@@ -5,9 +5,11 @@ using UnityEngine.UI;
 public class SliderScript : MonoBehaviour
 {
     private Slider slider;
+    
     private void Awake()
     {
         slider = GetComponent<Slider>();
+        slider.value = 100f;
     }
     public void Slider_Update(float var)
     {
